196ff18849b501e681bc7a2fd138db533a1b9b50
devesh <48615331+djpredator@users.noreply.github.com>
2021-09-04 12:42:17 +0000

    update-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    DemoComponent 
   ],
   imports: [
     BrowserModule
